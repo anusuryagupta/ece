ffd49b4272feffbb9dd9c4534fdcd035e3ead558
appleinfusedrice <appleinfusedrice@outlook.com>
2025-04-15 10:54:12 -0500

    Update bikelock.vhd
    handled S0 and S3

--- project/bikelock.vhd
+++ project/bikelock.vhd
@@ -34,29 +34,74 @@ library xil_defaultlib;
 use xil_defaultlib.all;
 
 entity bikelock is
-  Port (   num : in STD_LOGIC_VECTOR (3 downto 0); 
-           segment : out STD_LOGIC_VECTOR (6 downto 0);  -- A at 6, G at 0
-           anodedp : out STD_LOGIC_VECTOR (4 downto 0)); -- dp at 4, anode at 3 to 0
+  Port (   clk : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           invector : in STD_LOGIC_VECTOR(4 downto 0); -- x4 x3 x2 x1 x0
+           masterout : out STD_LOGIC_VECTOR(3 downto 0);
+           stateout : out STD_LOGIC_VECTOR(5 downto 0));
 end bikelock;
 
 architecture Behavioral of bikelock is
 
+type state IS (S0, S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12, S13, S14, S15, S16);
+  -- reset, armed, alarm, counter1, counter2, counter3, counter4, correct5, 
+  --counterfalse1, counterfalse2, counterfalse3, counterfalse4, counterfalse3, 
+  --counterfalsetwo2, counterfalsetwo3, counterfalsetwo4, counterfalsetwo5
+signal current_state, next_state : state;
+signal inputs : std_logic_vector(7 downto 0);
+signal reset2 : in std_logic;
+
 begin
-with num select
-    segment(6) <= '1' when "0001" | "0100" | "1011" | "1101", '0' when others; -- A
-with num select
-    segment(5) <= '1' when "0101" | "0110" | "1011" | "1100" | "1110" | "1111", '0' when others; -- B
-with num select
-    segment(4) <= '1' when "0010" | "1100" | "1110" | "1111", '0' when others; -- C
-with num select
-    segment(3) <= '1' when "0001" | "0100" | "0111" | "1010" | "1111", '0' when others; -- D
-with num select
-    segment(2) <= '1' when "0001" | "0011" | "0100" | "0101" | "0111" | "1001",'0' when others; -- E
-with num select
-    segment(1) <= '1' when "0001" | "0010" | "0011" | "0111" | "1101", '0' when others; -- F
-with num select
-    segment(0) <= '1' when "0000" | "0001" | "0111" | "1100", '0' when others; -- G
-anodedp <= "11110";
+  process(clk, invector, inputs, reset, reset2)
+  begin
+    if reset = '1' then
+        next_state <= S0;
+        inputs <= "10000000";
+      elsif reset2 = '1' then
+        next_state <= S0;
+        reset2 <= '0';
+        inputs <= "10000000";
+      elsif rising_edge(clk) then
+        current_state <= next_state;
+      end if;  
+    inputs(4 downto 0) <= invector;
+    case current_state is
+        when S0 => -- reset
+          case inputs is
+            when '10000000' | '10010000' => -- x4 or nobtn
+              next_state <= S0; -- stay in RESET
+              inputs(7 downto 5) <= '100';
+            when '10000100' => -- x2
+              next_state <= S3; -- goto counter1
+              inputs(7 downto 5) <= '100';
+            when '10001000' | '10000010' | '10000001' => -- x3, x1, x0
+              next_state <= S8; -- goto counterfalse1
+              inputs(7 downto 5) <= '100';
+            when others =>
+              reset2 <= 1; -- big nono
+          end case;
+        when S3 =>
+            case inputs is
+              when '10000000' | '01000000' | '00100000' =>
+                next_state <= S3; -- stay in counter1, outvector doesn't change
+              when '10000001' =>
+                next_state <= S4; -- goto counter2, outvector doesn't change
+              when '00100010' | '00100100' | '00101000' | '00110000' =>
+                next_state <= S2; -- goto ALARM, outvector doesn't change
+              when '10000100' | '01000100' =>
+                next_state <= S13; -- goto counterfalsetwo2, outvector doesn't change
+              when '10010000' =>
+                next_state <= S0; -- goto RESET
+              when '01010000' =>
+                next_state <= S1; -- goto ARMED
+              when '10001000' | '10000010' | '10001000' | '10000010' =>
+                next_state <= S9; -- goto counterfalse2, outvector doesn't change
+              when others =>
+                reset2 <= 1; -- big nono
+              end case;
+              
+      
+      
 
 end Behavioral;
  
