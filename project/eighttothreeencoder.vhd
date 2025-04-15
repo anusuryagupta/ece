e5d18de99b07699e74b6286dc274681727c51d0b
appleinfusedrice <appleinfusedrice@outlook.com>
2025-04-14 23:08:11 -0500

    Update eighttothreeencoder.vhd

--- project/eighttothreeencoder.vhd
+++ project/eighttothreeencoder.vhd
@@ -34,7 +34,7 @@ library xil_defaultlib;
 use xil_defaultlib.all;
 
 entity eighttothreeencoder is
-  Port (   I : in STD_LOGIC_VECTOR (7 downto 0); -- A B C x0 x1 x2 x3 x4, the x's should be priorityencoder(thatnumber)
+  Port (   I : in STD_LOGIC_VECTOR (7 downto 0); -- A B C x4 x3 x2 x1 x0, the x's should be priorityencoder(thatnumber)
            O : out STD_LOGIC_VECTOR (4 downto 0)); -- binary input that represents that input group
 end eighttothreeencoder;
 
@@ -45,11 +45,11 @@ with num select
 with num select
     O(3) <= '1' when "10010000" | "10001000" | "10000100" | "10000010" | "10000001" | "10000000", '0' when others; -- master state 0
 with num select
-    O(2) <= '1' when "10000001" | "01000001" | "00100001", '0' when others; -- x button 2
+    O(2) <= '1' when "10010000" | "01010000" | "00110000", '0' when others; -- x button 2
 with num select
-    O(1) <= '1' when "10000010" | "01000010" | "00100010" | "10000100" | "01000100" | "00100100", '0' when others; -- x button 1
+    O(1) <= '1' when "10001000" | "01001000" | "00101000" | "10000100" | "01000100" | "00100100", '0' when others; -- x button 1
 with num select
-    O(0) <= '1' when "10001000" | "01001000" | "00101000" | "10010000" | "01010000" | "00110000", '0' when others; -- x button 0
+    O(0) <= '1' when "10000010" | "01000010" | "00100010" | "10000001" | "01000001" | "00100001", '0' when others; -- x button 0
 
 end Behavioral;
  
